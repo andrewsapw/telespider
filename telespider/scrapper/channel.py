d123ec73551360c80a9e179de7641892170e8707
andrewsap <andrewsapw@gmail.com>
2023-04-20 09:30:11 +0300

    fix: handle `UsernameInvalid` error

--- telespider/scrapper/channel.py
+++ telespider/scrapper/channel.py
@@ -1,7 +1,10 @@
 from typing import AsyncGenerator
 
 from pyrogram.types import Message
-from pyrogram.errors.exceptions.bad_request_400 import UsernameNotOccupied
+from pyrogram.errors.exceptions.bad_request_400 import (
+    UsernameNotOccupied,
+    UsernameInvalid,
+)
 
 from telespider.config import settings
 from telespider.app import app
@@ -16,8 +19,8 @@ async def parse_channel(
     try:
         async for message in app.get_chat_history(channel_name, limit=limit):
             yield message
-    except UsernameNotOccupied:
-        logger.error(f"Skipping {channel_name} - does not exists")
+    except (UsernameNotOccupied, UsernameInvalid):
+        logger.error(f"Skipping {channel_name} - does not exists or invalid")
         return
 
 
